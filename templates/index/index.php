e0ff6b4d1af05bbb2bb4c143c671a9067442c10e
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-05-18 00:20:27 -0300

    Update index.php

--- templates/index/index.php
+++ templates/index/index.php
@@ -4,6 +4,7 @@
   <body>
   	<?php $this->include('public/presentation'); ?>
     <?php $this->include('overall/topnav'); ?>
+    
     <?php $this->include('overall/footer'); ?>
   </body>
 </html>
