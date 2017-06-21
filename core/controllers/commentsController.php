f1117bf5316397e714e3b531520f8c6cc5f3acc0
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-06-21 09:20:14 -0300

    Se añadio el controlador de los comentarios

---
+++ core/controllers/commentsController.php
@@ -0,0 +1,28 @@
+<?php
+
+# Security
+defined('INDEX_DIR') OR exit(APP . ' software says .i.');
+//------------------------------------------------
+
+class commentsController extends Controller {
+	public function __construct() {
+    	parent::__construct(true);
+    	if($this->sessions->isLoggedIn()) {
+    		$categorias = new Comments();
+    			switch ($this->router->getMethod()) {
+		        case 'add':
+            	if ($_POST) {
+              	$categorias->Add();
+            	}
+              break;
+						case 'addAnswer':
+            	if ($_POST) {
+              	$categorias->AddAnswer();
+            	}
+              break;
+          }
+    	}
+    }
+}
+
+?>
