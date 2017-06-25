61cc90258c142392f3aaa4925c7b33b978f6f45f
lucasdc <lucasdicunzolo@gmail.com>
2017-06-25 01:54:27 -0300

    Se agrego funcion para trabajo con postulantes

--- core/bin/functions/Postulantes.php
+++ core/bin/functions/Postulantes.php
@@ -1,5 +1,19 @@
 <?php
 
+function AllPostulantes() {
+  $db = new Connection();
+  $data = $db->select('*', 'Postulantes');
+  if(!$data) return false;
+
+  for($i = 0; $i < count($data); $i++) {
+    $postulantes[$i] = array(
+      'idUser' => $data[$i]['idUser'],
+      'idGauchada' => $data[$i]['idGauchada']
+    );
+  }
+  return $postulantes;
+}
+
 function Postulantes($idGauchada) {
   $db = new Connection();
   $data = $db->select('*', 'Postulantes p INNER JOIN Users u ON(p.idUser = u.idUser)', 'idGauchada='.$idGauchada);
@@ -8,12 +22,13 @@ function Postulantes($idGauchada) {
   for($i = 0; $i < count($data); $i++) {
     $postulantes[$i] = array(
       'idUser' => $data[$i]['idUser'],
+      'idGauchada' => $data[$i]['idGauchada'],
       'completeName' => $data[$i]['name'] . ' ' . $data[$i]['surname'],
       'email' => $data[$i]['email'],
       'profilePicture' => Images()[$data[$i]['idImage']]['path']
     );
   }
-  return $postulantes ?? false;
+  return $postulantes;
 }
 
 function Postulante($idGauchada, $idUser) {
