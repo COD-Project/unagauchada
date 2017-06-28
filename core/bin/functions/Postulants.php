f21739f62e61c768024e30a7842875781371f596
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-06-27 21:49:45 -0300

    Se modifico SelectedPostulant

--- core/bin/functions/Postulants.php
+++ core/bin/functions/Postulants.php
@@ -35,7 +35,18 @@ function SelectedPostulant($idGauchada){
   $where = 'idGauchada='.$idGauchada.' AND selected=1';
   $data = $db->select('*', 'Postulants p INNER JOIN Users u ON(p.idUser = u.idUser)', $where);
 
-  return (!$data) ? false : true;
+  if (!$data) return false;
+
+  for($i = 0; $i < count($data); $i++) {
+    $postulantes[$i] = array(
+      'idUser' => $data[$i]['idUser'],
+      'idGauchada' => $data[$i]['idGauchada'],
+      'completeName' => $data[$i]['name'] . ' ' . $data[$i]['surname'],
+      'email' => $data[$i]['email'],
+      'profilePicture' => Images()[$data[$i]['idImage']]['path']
+    );
+  }
+  return $postulantes;
 }
 
 ?>
