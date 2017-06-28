75c063577a22d6195a2262547c26f00a03b0c52a
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-06-28 09:28:07 -0300

    Se añadio un mensaje de success por get

--- templates/public/presentation.php
+++ templates/public/presentation.php
@@ -9,6 +9,12 @@
     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
     <p class="text-fluid"><strong>' . $_GET['error'] . '</strong></p>
     </div></div>';
+  } else if (isset($_GET['success'])) {
+    echo '<div class="container pt-6 text-center wow fadeIn" data-wow-delay="0.2s">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <p class="text-fluid"><strong>' . $_GET['success'] . '</strong></p>
+    </div></div>';
   }
 ?>
 
