a9ad9b1777af6b7c006e487745f8363e62060899
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-06-29 07:42:35 -0300

    Modificación del parametro success del GET

--- core/models/class.Gauchadas.php
+++ core/models/class.Gauchadas.php
@@ -75,7 +75,7 @@ final class Gauchadas extends Models
 				));
 			}
 	    $this->db->update('Users', array('credits' => (Sessions::getInstance())->connectedUser()['credits'] - 1), 'idUser='.(Sessions::getInstance())->connectedUser()['idUser'], 'LIMIT 1');
-	    Func::redirect(URL . '?success=true');
+	    Func::redirect(URL . '?success=¡Se creo la gauchada!');
   	}
 
   	final public function delete() {
