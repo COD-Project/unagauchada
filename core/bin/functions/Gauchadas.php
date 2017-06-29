3704c6088b572c066a78ce50f2c416ad9b6c72ac
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-06-29 11:14:14 -0300

    Se arreglo la funcion GauchadasDebit

--- core/bin/functions/Gauchadas.php
+++ core/bin/functions/Gauchadas.php
@@ -45,7 +45,10 @@ function Gauchadas($all = false) {
 
 function GauchadasDebit($idUser) {
   $db = new Connection();
-  $data = $db->select('*', 'Gauchadas g INNER JOIN Postulants p ON (g.idGauchada=p.idGauchada)', 'g.idUser='.$idUser.' AND p.selected=1 AND g.validate IS NULL');
+  $from = 'Gauchadas g INNER JOIN Postulants p ON (g.idGauchada=p.idGauchada)';
+  $subquery = '(SELECT idGauchada FROM Ratings)';
+  $where = 'g.idUser='.$idUser.' AND p.selected=1 AND g.validate IS NULL AND g.idGauchada NOT IN '. $subquery;
+  $data = $db->select('*', $from, $where);
   if(!$data) return false;
   return true;
 }
