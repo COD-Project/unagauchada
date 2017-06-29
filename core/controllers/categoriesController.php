75257fa2407669bffd13a7d62f00ebb73ad582ff
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-06-29 15:12:56 -0300

    Se renombro el controlador

---
+++ core/controllers/categoriesController.php
@@ -0,0 +1,38 @@
+<?php
+
+# Security
+defined('INDEX_DIR') OR exit(APP . ' software says .i.');
+//------------------------------------------------
+
+class categoriesController extends Controller {
+	public function __construct() {
+    	parent::__construct(true);
+    	if($this->sessions->isLoggedIn()) {
+    		$categories = new Categories();
+    		if($this->sessions->isGranted()){
+    			switch ($this->router->getMethod()) {
+    		        case 'add':
+  		          	if ($_POST) {
+  		            	$categories->add();
+  		          	} 
+                  break;
+                case 'edit':
+                  if ($_POST) {
+                    $categories->edit();
+                  }
+                  break;
+                case 'delete':
+                  if($this->router->getId() != 1){
+                    $categories->delete();
+                  }
+                  break;
+                case 'main':
+                  $this->render('categories/main');
+                  break;
+                }
+            }
+    	}
+    }
+}
+
+?>
