fc443c00106ddaefcea8f9951e01e4459326e581
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-07-11 11:24:19 -0300

    Quedo mal el nombre de la imagen, ahora es logo.jpg

--- templates/overall/topnav.php
+++ templates/overall/topnav.php
@@ -7,7 +7,7 @@
     </button>
     <div class="container">
       <a class="navbar-brand" href="<?php echo URL; ?>">
-          <img src="views/app/images/avatar_28012e5b8492_128.png" class="d-inline-block align-top cmd_zoomin" style="height: 35px;" alt="UnaGauchada">
+          <img src="views/app/images/logo.png" class="d-inline-block align-top cmd_zoomin" style="height: 35px;" alt="UnaGauchada">
           <span style="color: coral;"><strong>Una</strong>Gauchada</span>
       </a>
       <div class="collapse navbar-collapse" id="navbarNav">
