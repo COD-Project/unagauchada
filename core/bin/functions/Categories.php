59b31cd55ffd2627dc5c25ad82d55c03ae40f027
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-07-11 11:02:58 -0300

    Se agrego una nueva funcion a las categorias

--- core/bin/functions/Categories.php
+++ core/bin/functions/Categories.php
@@ -2,7 +2,7 @@
 
 function Categories($id = null) {
   $db = new Connection();
-  $where = $id==null ? '1=1 AND validate IS NULL' : "idCategory=$id AND validate IS NULL";
+  $where = $id==null ? '1=1 AND validate=0' : "idCategory=$id AND validate=0";
   $data = $db->select('*', 'Categories', $where);
   if(!$data) return false;
 
@@ -10,6 +10,22 @@ function Categories($id = null) {
     $categories[$i] = array(
       'idCategory' => $data[$i]['idCategory'],
       'name' => $data[$i]['name']
+    );
+  }
+  return $categories;
+}
+
+function CategoriesExist($name) {
+  $db = new Connection();
+  $where = 'name LIKE "'.$name.'"';
+  $data = $db->select('*', 'Categories', $where);
+  if(!$data) return false;
+
+  for($i = 0; $i < count($data); $i++) {
+    $categories[$i] = array(
+      'idCategory' => $data[$i]['idCategory'],
+      'name' => $data[$i]['name'],
+      'validate' => $data[$i]['validate']
     );
   }
   return $categories;
