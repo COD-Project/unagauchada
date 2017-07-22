2874df44a5bec1ad9cd485d9697df7599a255be7
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-07-22 01:41:02 -0300

    Se modifico la visual

--- templates/categories/add.php
+++ templates/categories/add.php
@@ -5,7 +5,7 @@
       <div class="wrap">
           <form name="" class="form-inline" action="categories/add" method="post">
             <label class="sr-only" for="name">Añadir categoria</label>
-            <input type="text" name="name" style="margin-right:1.5rem;" class="form-control" for="name" placeholder="Nombre de la categoria">
+            <input type="text" name="name" style="margin-right:1.5rem;" class="form-control" for="name" placeholder="Nombre de la categoria" data-toggle="validator" required="true">
 
             <button type="submit" class="btn btn-primary">Añadir</button>
           </form>
