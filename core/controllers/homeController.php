4556a2f02a36eeeafbe4f3f42a5f3146d84ec062
ulises-jeremias <ulisescf.24@gmail.com>
2017-07-22 04:58:02 -0300

    Cambios

--- core/controllers/homeController.php
+++ core/controllers/homeController.php
@@ -7,12 +7,16 @@ defined('INDEX_DIR') OR exit(APP . ' software says .i.');
 class homeController extends Controller {
   public function __construct() {
     parent::__construct();
+    $this->initialize();
+    $this->render('index/index');
+  }
+
+  private function initialize() {
     $this->models = array(
       'gauchadas' => new Gauchadas,
       'categories' => new Categories
     );
     $this->gauchadas = $this->models['gauchadas']->get();
-    $this->render('index/index');
   }
 }
 
