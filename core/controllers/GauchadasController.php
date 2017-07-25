b408c46a5bb0368102412dd6d9346e3226cf802e
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-07-25 03:55:20 -0300

    Fix

---
+++ core/controllers/GauchadasController.php
@@ -0,0 +1,74 @@
+<?php
+
+# Security
+defined('INDEX_DIR') OR exit(APP . ' software says .i.');
+//------------------------------------------------
+
+class GauchadasController extends Controller {
+  public function __construct() {
+    parent::__construct(true);
+    if($this->sessions->isLoggedIn()) {
+      switch ($this->router->getMethod()) {
+      case 'add':
+        if(!$this->sessions->isGranted()) {
+          $this->categories = (new Categories)->get();
+          if ($_POST) {
+            $this->models['gauchadas']->add();
+          } else if(!$this->debit()){
+            $this->render('gauchadas/add');
+          } else {
+            Func::redirect(URL.'?success=Usted tiene calificaciones pendientes.');
+          }
+        }
+        break;
+      case 'view':
+        if (array_key_exists($this->router->getId(), $this->gauchadas)) {
+          $this->gauchada = $this->gauchadas[$this->router->getId()];
+          $this->render("gauchadas/view");
+        } else {
+          Func::redirect();
+        }
+        break;
+      case 'delete':
+        if (array_key_exists($this->router->getId(), $this->gauchadas)) {
+          $this->models['gauchadas']->delete();
+        } else {
+          Func::redirect();
+        }
+        break;
+      default:
+        Func::redirect();
+      }
+    } else if ($this->router->getMethod() == 'view') {
+      Func::redirect(URL . '?error=Debes estar logueado para ver una gauchada');
+    }
+  }
+
+  protected function init() {
+    $this->setModels(array(
+      "gauchadas",
+      "postulants",
+      "users"
+    ));
+    $this->gauchadas = $this->models['gauchadas']->get(['all']);
+    $this->postulants = $this->models['postulants']->get(["gauchada" => $this->router->getId()]);
+    $this->users = $this->models["users"]->get();
+    $this->selected = $this->models["postulants"]->get([
+      "gauchada" => $this->router->getId(),
+      "selected" => "1"
+    ]);
+  }
+
+  private function debit() {
+    $db = new Connection();
+    $idUser = (Sessions::getInstance())->connectedUser()['idUser'];
+    $from = 'Gauchadas g INNER JOIN Postulants p ON (g.idGauchada=p.idGauchada)';
+    $subquery = '(SELECT idGauchada FROM Ratings)';
+    $where = 'g.idUser='.$idUser.' AND p.selected=1 AND g.validate IS NULL AND g.idGauchada NOT IN '. $subquery;
+    $data = $db->select('*', $from, $where);
+    if(!$data) return false;
+    return true;
+  }
+}
+
+?>
---
+++ core/controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+# Security
+defined('INDEX_DIR') OR exit(APP . ' software says .i.');
+//------------------------------------------------
+
+class HomeController extends Controller {
+  public function __construct() {
+    parent::__construct();
+    $this->render('index/index');
+  }
+
+  protected function init() {
+    $this->setModels(array(
+      'gauchadas',
+      'categories'
+    ));
+    $this->gauchadas = $this->models['gauchadas']->get();
+    $this->categories = $this->models['categories']->get();
+  }
+}
+
+?>
