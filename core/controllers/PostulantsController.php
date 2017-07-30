40d5419ca8608b31d724aaa491c7fca7df8e9d2f
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-07-29 21:30:44 -0300

    Cambios

--- core/controllers/PostulantsController.php
+++ core/controllers/PostulantsController.php
@@ -5,6 +5,7 @@ defined('INDEX_DIR') OR exit(APP . ' software says .i.');
 //------------------------------------------------
 
 class PostulantsController extends Controller {
+
   public function __construct() {
     parent::__construct(true);
     if (!array_key_exists($this->router->getId(), $this->gauchadas)) {
@@ -16,31 +17,6 @@ class PostulantsController extends Controller {
                ->getMethod()
         ]);
     }
-  }
-
-  protected function init() {
-    $this->setModels([
-      "users",
-      "postulants",
-      "gauchadas"
-    ]);
-
-    $this->user = $this->models["users"]
-                       ->get()[
-                           $this->router
-                                ->getId()
-                         ];
-
-    $this->postulants = $this->models["postulants"]
-                             ->get([
-                                 "gauchada" => $this->router
-                                                    ->getId(),
-                                 "user" => $this->sessions
-                                                ->connectedUser()['idUser']
-                               ]);
-
-    $this->gauchadas = $this->models["gauchadas"]
-                            ->get();
   }
 
 }
