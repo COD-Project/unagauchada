ca9a83d72605b8cdef1a5a50ba94054930913c77
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-07-30 23:00:57 -0300

    Some changes

--- templates/analytics/analytics.php
+++ templates/analytics/analytics.php
@@ -1,5 +1,5 @@
 <div class="row mb-3">
-    <div class="col-xl-3 col-lg-6 wow fadeIn" data-wow-delay="0.2s">
+    <div class="col-xl-3 col-lg-6 wow fadeIn" data-wow-delay="0.15s">
         <div class="card card-inverse card-success">
             <div class="card-block bg-success">
                 <div class="rotate">
@@ -10,7 +10,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-3 col-lg-6 wow fadeIn" data-wow-delay="0.4s">
+    <div class="col-xl-3 col-lg-6 wow fadeIn" data-wow-delay="0.2s">
         <div class="card card-inverse card-danger">
             <div class="card-block bg-danger">
                 <div class="rotate">
@@ -21,7 +21,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-3 col-lg-6 wow fadeIn" data-wow-delay="0.8s">
+    <div class="col-xl-3 col-lg-6 wow fadeIn" data-wow-delay="0.25s">
         <div class="card card-inverse card-info">
             <div class="card-block bg-info">
                 <div class="rotate">
@@ -32,7 +32,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-3 col-lg-6 wow fadeIn" data-wow-delay="1.0s">
+    <div class="col-xl-3 col-lg-6 wow fadeIn" data-wow-delay="0.3s">
         <div class="card card-inverse card-warning">
             <div class="card-block bg-warning">
                 <div class="rotate">
@@ -48,9 +48,9 @@
 
 <hr/>
 
-<div class="row">
+<div class="row wow fadeIn" data-wow-delay="0.4s">
   <div class="col-md-3 col-lg-2" role="navigation">
-    <ul class="nav nav-tabs flex-column pl-1">
+    <ul class="nav nav-pills nav-fill flex-column pl-1">
         <li class="nav-item"><a data-target="#principal" data-toggle="tab" class="nav-link active">Principal</a></li>
         <li class="nav-item"><a class="nav-link">Estadísticas</a></li>
         <li class="nav-item"><a class="nav-link">Usuarios</a></li>
@@ -61,7 +61,7 @@
   <div class="col-md-9 col-lg-10">
     <div class="tab-content p-b-3">
       <div class="tab-pane active wow fadeIn" id="principal" data-wow-delay="0.1s">
-asd
+        asd
       </div>
     </div>
   </div>
