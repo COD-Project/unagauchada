010a883b74c175e6318eac31904bfbee803ac9ad
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-07-31 21:08:30 -0300

    Cambios

--- core/kernel/Connection.php
+++ core/kernel/Connection.php
@@ -134,7 +134,25 @@ final class Connection extends PDO {
 
   final private function purifier($elem)
   {
-    return str_replace(array('<script>','</script>','<script src','<script type='), '', $elem);
+    return str_replace(['applet', 'base', 'bgsound', 'blink', 'embed', 'expression',
+  'frame', 'javascript', 'layer', 'link', 'meta', 'object', 'onabort', 'onactivate',
+  'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut',
+  'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint',
+  'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange',
+  'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect',
+  'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete',
+  'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave',
+  'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate',
+  'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout',
+  'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete',
+  'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave',
+  'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup',
+  'onmousewheel', 'onmove', 'onmoveend', 'onmovestart',
+  'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset',
+  'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit',
+  'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange',
+  'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload',
+  'script', 'style', 'title', 'vbscript', 'xml'], '', $elem);
   }
 
   /**
