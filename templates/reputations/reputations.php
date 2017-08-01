1207d1ca70e6101400c8328c00a887ae6420264f
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2017-08-01 17:34:50 -0300

    Pero mira esa visual!

--- templates/reputations/reputations.php
+++ templates/reputations/reputations.php
@@ -12,15 +12,15 @@
         <tbody>
           <?php
           for ($i=0; $i < count($this->reputations); $i++) {
-              $reputations = $this->reputations[$i];
-              $reputations['min_bound'] = $i == 0 ? "-&infin;" : $reputations['min_bound'];
-              $reputations['max_bound'] = $i == count($this->reputations) ? "&infin;" : $reputations['max_bound'];
+              $reputation = $this->reputations[$i];
+              $reputation['min_bound'] = $i == count($this->reputations) - 1 ? "<strong>- &infin;</strong>" : $reputation['min_bound'];
+              $reputation['max_bound'] = $i == 0 ? "<strong>&infin;</strong>" : $reputation['max_bound'];
               echo("
               <tr>
                   <td>" . ($i + 1) . "</td>
-                  <td>" . $reputations['name'] . "</td>
-                  <td>" . $reputations['min_bound'] . "</td>
-                  <td>" . $reputations['max_bound'] . "</td>
+                  <td>" . $reputation['name'] . "</td>
+                  <td>" . $reputation['min_bound'] . "</td>
+                  <td>" . $reputation['max_bound'] . "</td>
                   <td>
                     <a href=\"reputations/edit/" . $reputation['idReputation'] . "\">
                       <button type=\"button\" class=\"btn btn-danger btn-circle\">
