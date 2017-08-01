d11da5b5897698287d1bcefb767963cc66358797
ulises-jeremias <ulisescf.24@gmail.com>
2017-08-01 17:23:25 -0300

    Reputations

---
+++ templates/settings/settings.php
@@ -0,0 +1,11 @@
+<div style="margin-top: 75px">
+  <div class="row">
+    <div class="col-md-3">
+  asd
+    </div>
+    <div class="col-md-9">
+      <?php $this->include('reputations/reputations'); ?>
+    </div>
+  </div>
+
+</div>
