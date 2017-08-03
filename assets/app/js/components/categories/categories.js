329a7e718e26d026424f2e4d9147f9207f933c23
Juan Ocampos <juancruz.ocampos@synapse.com>
2017-08-02 21:48:00 -0300

    arreglo de las categorias (en proceso)

--- assets/app/js/components/categories/categories.js
+++ assets/app/js/components/categories/categories.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-  if (Func.$('#categoriasAdd')) {
-    cmdForm.render({
-      elem: Func.$('#categoriasAdd'),
-      inputs: [{
-        name: 'name',
-        inner: 'Nombre',
-        type: 'text'
-      }],
-      action: 'categorias/add'
-    });
+  if(Func.$("#edit_category")) {
+    var buttons;
+    if(buttons = Func.$(".edit")) {
+      for (var i = buttons.length - 1; i >= 0; i--) {
+        buttons[i].onclick = function(event) {
+          event.preventDefault();
+          var href = this.href;
+          var div_col_1 = this.parentNode;
+          var li = div_col_1.parentNode;
+          var category_name = li.children[2].innerHTML;
+          console.log(category_name);
+          var div_col_7 = li.children[2];
+          div_col_7.innerHTML = 
+          `
+          <form class="form-inline" action="${href}" method="post">
+            <input type="text" name="name" placeholder=${category_name} required>
+            <button type="submit" class="btn btn-primary rounded-circle"><i class="fa fa-arrow-right"></i></button>
+          </form>
+          `
+
+        };
+      }
+    }
   }
 });
--- templates/settings/settings.php
+++ templates/settings/settings.php
@@ -1,12 +1,12 @@
 <div style="margin-top: 75px">
   <div class="row wow fadeIn" data-wow-delay="0.1s">
     <div class="col-md-3">
-      <h3>Categorías</h3>
+      <h3>Categorías</h3> 
     </div>
-    <div class="col-md-9">
-
+    <div class="col-md-9">    
     </div>
   </div>
+  <?php $this->include('categories/categories'); ?>
   <div style="height: 75px;"></div>
   <div class="row wow fadeIn" data-wow-delay="0.15s">
     <div class="col-md-3">
