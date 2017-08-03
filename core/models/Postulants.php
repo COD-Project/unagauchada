92f7718a56b26edc4f9cdea396c0257e354a0a6a
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-08-03 01:20:20 -0300

    Cambios

--- core/models/Postulants.php
+++ core/models/Postulants.php
@@ -91,7 +91,7 @@ final class Postulants extends Models
             $where .= " AND r.idRating " . ($options['ranked'] ? " IS NOT NULL" : "IS NULL");
         }
         return ([
-          "elements" => "p.*, r.idRating, p.idUser as idPostulant",
+          "elements" => "p.*, p.idUser as idPostulant" . (isset($options['ranked']) ? ", r.idRating" : ""),
           "table" => $table ,
           "where" => $where
         ]);
